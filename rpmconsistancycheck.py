372d9c62b605780c076c29ee406ec2eec7b0fa86
chrisp <cprocter@redhat.com>
2013-11-27 23:21:54 +0000

    bunch of changes to a) deal withrepos better b) be more efficient c) have clearer output

--- rpmconsistancycheck.py
+++ rpmconsistancycheck.py
@@ -14,9 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-# seth vidal 2005 (c) etc etc
-
-
 
 import os
 import sys
@@ -27,40 +24,46 @@ from optparse import OptionParser
 rpmlist=[]
 filename=""
 parser = OptionParser()
-parser.add_option("-f", "--file", default=None, dest="filename", help='file containing list of rpms')
+parser.add_option("-f", "--file", default=None, dest="filename", help='file containing list of rpms to check')
 parser.add_option("-e", "--errata", action="append", default=None, dest="errata", help='file containing list of rpms belonging to an errata')
 parser.add_option("-d", "--dir", default=None, dest="dir", help='dir containing errata rpms')
-parser.add_option("-i", "--install", default=None, action="store_const",const=1, dest="installnew", help='install errata packages not already installed')
-parser.add_option("-r", "--repo", action="append", default=None, dest="repolist", help='file containing list of rpms belonging to an errata')
-#parser.add_option("-f", "--file", default=None, help='file containing list of rpms')
+parser.add_option("-r", "--repo", action="append", default=None, dest="repolist", help='id of a repo to check against')
 
 (opt,args) = parser.parse_args()
-#print opt.filename
+filename = opt.filename or sys.exit(1)
+repolist = opt.repolist or sys.exit(1)
+errata = opt.errata or ""
+rpmdir = opt.dir or "."
 
 ## read the rpms from a file
 #rpmlist = [line.rstrip('\n') for line in open(opt.filename)]
-for line in open(opt.filename):
+for line in open(filename):
 	rpmlist.append(line.rstrip('\n'))
 
 erratapkgs=[]
-for e in opt.errata:
+for e in errata:
 	for line in open(e):
-		erratapkgs.append(yum.packages.YumLocalPackage(filename=opt.dir+"/"+line.rstrip('\n')))
+		erratapkgs.append(yum.packages.YumLocalPackage(filename=rpmdir+"/"+line.rstrip('\n')))
 
 yb = yum.YumBase()
-yb.setCacheDir()
+yb.setCacheDir("/var/cache/yum/")
+
+conrepos=[]
+for i in yb.repos.findRepos("*"):
+	conrepos.append(i.getAttribute("id"))
+
 
 yb.repos.disableRepo('*')
 
 #repolist=[ "fedora-clone","updates-clone" ]
-for repo in opt.repolist:
-	repodetails=repo.split(",")
-	newrepo = yum.yumRepo.YumRepository(repodetails[0])
-	newrepo.baseurl = repodetails[1]
-	newrepo.metadata_expire = 0
-	newrepo.timestamp_check = False
-	yb.repos.add(newrepo)
-	yb.repos.enableRepo(repodetails[0])
+for repo in repolist:
+	if repo not in conrepos:
+		newrepo = yum.yumRepo.YumRepository(repo)
+		newrepo.metadata_expire = 0
+		newrepo.timestamp_check = False
+		yb.repos.add(newrepo)
+	
+	yb.repos.enableRepo(repo)
 
 yb.pkgSack = yb.repos.populateSack(mdtype='metadata',cacheonly=1) #which='enabled',
 
@@ -70,61 +73,49 @@ for i in yb.pkgSack:
 	pkgobjlist["%s-%s-%s.%s"%(i.name, i.version, i.release,i.arch)]=i
 
 instpkgobjs = dict()
-for rpm in rpmlist:
+for rpmname in rpmlist:
 	#print rpm
-	rpmname=pkgobjlist.get(rpm,"")
-	if rpmname =="":
-		print "NOT FOUND IN REPOs: " +rpm
-		continue
+	rpm = pkgobjlist.get(rpm,"")
+	if rpm =="":
+		try:
+			rpm = yum.packages.YumLocalPackage(filename= rpmdir + "/" + rpmname + ".rpm")
+		except:
+			print "NOT FOUND IN REPOs: " +rpmname
+			continue
 	#instpkgobjs.append(rpmname)
-	instpkgobjs[rpmname.name]=rpmname
-	
+	instpkgobjs[rpm]=1
+
+
 
 ### replace any from the errata list that are newer then the installed package that matches e
-#errsack= yum.packageSack.PackageSack()
 for e in erratapkgs:
-	if instpkgobjs.get(e.name) and e.verGT(instpkgobjs[e.name]):
-		instpkgobjs[e.name]=e
-	elif instpkgobjs.get(e.name) and e.verLT(instpkgobjs[e.name]):
+	if instpkgobjs.get(e) and e.verGT(instpkgobjs[e]):
+		instpkgobjs[e]=1
+	elif instpkgobjs.get(e) and e.verLT(instpkgobjs[e]):
 		print "%s-%s-%s.%s older then installed"%(e.name, e.version, e.release,e.arch)
-	elif instpkgobjs.get(e.name) and e.verEQ(instpkgobjs[e.name]):
+	elif instpkgobjs.get(e) and e.verEQ(instpkgobjs[e]):
 		print "%s-%s-%s.%s already installed"%(e.name, e.version, e.release,e.arch)
-	elif instpkgobjs.get(e.name,"") == "" and opt.installnew:
-		instpkgobjs[e.name]=e
+	elif instpkgobjs.get(e,"") == "" and opt.installnew:
+		instpkgobjs[e]=1
 	else:
 		yb.pkgSack.addPackage(e)
-		#errsack.addPackage(e)
-
-#yb.repos.add(errsack)
-
 
 try:
-	deps = yb.findDeps(instpkgobjs.values())
+	deps = yb.findDeps(instpkgobjs.keys())
 except Exception as e:
-	for i in instpkgobjs.values():
-		print "%s=%s"%(i,type(i))
 	print e
 	sys.exit(0)
 
-pkgs=dict()
 for i in deps.keys():				### packages
 	for j in deps[i].keys():		### requirements for package
-		if deps[i][j] == []:
+		if deps[i][j] == []:		### no requirements
 			continue
 
 		for k in deps[i][j]:		### potential resolutions for requirements
-			name="%s-%s-%s.%s"%(k.name, k.version, k.release,k.arch)
-			if name in rpmlist:		### there is something installed that satisfies the dep
+			if instpkgobjs.get(k):
 				break
-			#elif pkgobjlist.get(name,"") != "":	### there is something in the repo that satisfies the dep
-			#	break
-		else:						### there is nothing available anywhere that satisfies the dep
-			print "%s-%s-%s.%s requires %s"%(i.name, i.version, i.release,i.arch," ".join([m.name for m in deps[i][j]]))
-			pkgs[name]=1
-
-print "================"
-for i in pkgs.keys():
-	print i
-
-
+		else:
+			print "%s-%s-%s.%s requires one of:"%(i.name, i.version, i.release,i.arch)
+			print "\t" + "\t\n".join(["%s-%s-%s.%s"%(m.name, m.version, m.release,m.arch) for m in instpkgobjs])
+				
 sys.exit(0)
